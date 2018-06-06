1c7b00502e0b185dec8cfa1cd55131c20a9e0c73
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-06-07 01:43:28 +0900

    parse post body string

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/mitubaEX/graphQL_sample/application/graphql_util"
 	"github.com/graphql-go/graphql"
+	"github.com/mitubaEX/graphQL_sample/application/graphql_util"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -21,9 +22,15 @@ func executeQuery(query string, schema graphql.Schema) *graphql.Result {
 
 func main() {
 	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
-		result := executeQuery(r.PostFormValue("query"), graphql_util.NewSchema())
+		defer r.Body.Close()
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+		result := executeQuery(fmt.Sprintf("%s", body), graphql_util.NewSchema())
 		json.NewEncoder(w).Encode(result)
 	})
 
+	fmt.Println("Now server is running on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
